b33cf6747abff27381b53bb3c525c5562805cb3e
uddeshyac9 <uddeshyac9@gmail.com>
2023-12-18 21:40:09 +0530

    Added Todo-App

---
+++ React-Folio/03-React-Advance/Todo-App-Redux/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import AddTodos from './components/AddTodos'
+import Todos from './components/Todos'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+ <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                    <AddTodos/>
+                       
+                    </div>
+                    <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <Todos/>
+                
+                    </div>
+                </div>
+            </div>
+    </>
+  )
+}
+
+export default App
