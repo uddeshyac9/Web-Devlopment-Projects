66f7ae2d3611abdd1404258e7da49d886ca26d86
uddeshyac9 <uddeshyac9@gmail.com>
2024-03-05 23:58:12 +0530

    some-cahnge

--- React-Folio/03-React-Advance/ShoppingCart-Redux/src/pages/Cart.jsx
+++ React-Folio/03-React-Advance/ShoppingCart-Redux/src/pages/Cart.jsx
@@ -32,7 +32,7 @@ function Cart() {
               </div>
   
               <div className="flex flex-col">
-                <p className="text-xl font-bold"><span className="text-gray-700 font-semibold">Total Amount:</span> ${totalAmount}</p>
+                <p className="text-xl font-bold"><span className="text-gray-700 font-semibold">Total Amount:</span> ${totalAmount.toFixed(2)}</p>
                 <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl">CheckOut Now</button>
               </div>
             </div>
